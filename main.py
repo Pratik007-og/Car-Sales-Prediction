3e367ffab50f2034f5582b5249f35a3f0d609a01
Pratik Patnaik <pratik.patnaik007@gmail.com>
2021-05-28 19:47:52 +0530

    final

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def predict():
         Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
         if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        else:
+            Fuel_Type_Petrol=1
+            Fuel_Type_Diesel=0
+        elif(Fuel_Type_Petrol == 'Diesel'):
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=1
         else:
@@ -37,14 +37,17 @@ def predict():
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
         else:
-            Seller_Type_Individual=0	
+            Seller_Type_Individual=0
+
         Transmission_Mannual=request.form['Transmission_Mannual']
         if(Transmission_Mannual=='Mannual'):
             Transmission_Mannual=1
         else:
             Transmission_Mannual=0
+
         prediction=model.predict([[Present_Price,Kms_Driven2,Owner,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
         output=round(prediction[0],2)
+
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
